d6f187c4c31df713b6947fc4037d352cb231ea6d
Jackson Collins-Ikpe <jacksoncikpe@gmail.com>
2022-10-22 22:12:36 +0100

    No now dynamic

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Remaining = () => {
+
+  const { budget, expenses } = useContext(AppContext);
+
+  const totalExpense = expenses.reduce((total, item) => {
+    return total + item.cost;
+  }, 0);
+
+  const alertType = totalExpense > budget ? 'alert alert-danger' : 'alert alert-success';
+
   return (
-    <div className="alert alert-success">
-        <span>Remaining: $5000</span>
+    <div className={alertType}>
+        <span>Remaining: ${budget - totalExpense}</span>
     </div>
   )
 }
